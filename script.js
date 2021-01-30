6487a10d4f0968eb811cbed51195d977ec26b232
tamim11 <mmmtamim@gmail.com>
2021-01-30 17:52:45 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+//--------- below 5 const is for event handling
+const firstClassPlus = document.getElementById("first-class-plus");
+const firstClassMinus = document.getElementById("first-class-minus");
+const economyPlus = document.getElementById("economy-plus");
+const economyMinus = document.getElementById("economy-minus");
+const bookNow = document.getElementById("book-now");
+
+//-------- below 5 const is for updating display
+const firstClassInput = document.getElementById("first-class-input");
+const economyInput = document.getElementById("economy-input");
+const subtotalAmount = document.getElementById("subtotal");
+const vatAmount = document.getElementById("vat");
+const totalAmount = document.getElementById("total");
+
+//-------- functions
+function updateSpanContent(targetSpan, updateValue) {
+    let currentContent = parseInt(targetSpan.innerText);
+    targetSpan.innerText = currentContent + updateValue;
+}
+
+function updateInputContent(targetInputField, updateAmount) {
+    let currentContent = parseInt(targetInputField.value);
+    targetInputField.value = currentContent + updateAmount;
+}
+
+function updateVatTotal(vat) {
+    vatAmount.innerText = vat;
+    totalAmount.innerText = parseInt(subtotalAmount.innerText) + vat;
+}
+
+function firstClassUpdater(plusOrMinus) {
+    updateInputContent(firstClassInput, plusOrMinus * 1);
+    updateSpanContent(subtotalAmount, plusOrMinus * 150);
+    let vat = parseInt(subtotalAmount.innerText) * 0.1; // 10% vat
+    updateVatTotal(vat);
+}
+
+function economyUpdater(plusOrMinus) {
+    updateInputContent(economyInput, plusOrMinus * 1);
+    updateSpanContent(subtotalAmount, plusOrMinus * 100);
+    let vat = parseInt(subtotalAmount.innerText) * 0.1; // 10% vat
+    updateVatTotal(vat);
+}
+
+//-------- event handlers
+firstClassPlus.addEventListener('click', () => {
+    firstClassUpdater(1);
+});
+
+firstClassMinus.addEventListener('click', () => {
+    let currentAmount = parseInt(firstClassInput.value);
+    // dont allow negative input
+    if (currentAmount == 0) {
+        return;
+    }
+    firstClassUpdater(-1);
+});
+
+economyPlus.addEventListener('click', () => {
+    economyUpdater(1);
+});
+
+economyMinus.addEventListener('click', () => {
+    let currentAmount = parseInt(economyInput.value);
+    // dont allow negative input
+    if (currentAmount == 0) {
+        return;
+    }
+    economyUpdater(-1);
+});
